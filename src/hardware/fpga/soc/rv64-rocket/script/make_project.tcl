8c0b78c0df5a2e6aba5e9fbb3b33aa01b2c45ab6
phthinh <hung3@e.ntu.edu.sg>
2021-07-06 10:02:06 +0100

    update the rv64 rocketchip soc

--- src/hardware/fpga/soc/rv64-rocket/script/make_project.tcl
+++ src/hardware/fpga/soc/rv64-rocket/script/make_project.tcl
@@ -6,9 +6,8 @@
 set project_name [lindex $argv 0]
 set bsp_dir 	 [lindex $argv 1]
 set work_dir 	 [lindex $argv 2]
-set part		 [lindex $argv 3]
-set target		 [lindex $argv 4]
-
+set board		 [lindex $argv 3]
+set part		 [lindex $argv 4]
 
 # Set the directory path for the original project from where this script was exported
 set orig_proj_dir [file normalize $work_dir/$project_name]
@@ -22,7 +21,6 @@ set proj_dir [get_property directory [current_project]]
 # Set project properties
 set obj [get_projects $project_name]
 set_property "default_lib" "xil_defaultlib" $obj
-#set_property "board_part" "Sasebo" $obj
 set_property "PART" $part $obj 
 set_property "simulator_language" "Mixed" $obj
 
@@ -32,20 +30,11 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 }
 
 # Set 'sources_1' fileset object
-set files [list \
-			[file normalize $work_dir/$project_name/rtl_sources/system_top_wrapper.v] \
-			[file normalize $work_dir/$project_name/rtl_sources/plusarg_reader.v] \
-			[file normalize $work_dir/$project_name/rtl_sources/AsyncResetReg.v] \
-			[file normalize $work_dir/$project_name/rtl_sources/EICG_wrapper.v] \
-			[file normalize $work_dir/$project_name/rtl_sources/rocketcore.v] \
-			[file normalize $work_dir/$project_name/rtl_sources/memcore.v] \
-			[file normalize $work_dir/$project_name/rtl_sources/cop_ise.v] \
-			[file normalize $work_dir/$project_name/rtl_sources/chacha_ise_v1.v] \
-			[file normalize $work_dir/$project_name/rtl_sources/chacha_ise_v2.v] \
-			[file normalize $work_dir/$project_name/rtl_sources/chacha_ise_v3.v] \
-			[file normalize $work_dir/$project_name/rtl_sources/chacha_ise_v4.v] \
-              ]
-
+set files [list]
+set vfiles [glob -directory $work_dir/$project_name/rtl_sources/ *.v]
+foreach item $vfiles {
+	lappend files [file normalize $item]
+}
 add_files -norecurse -fileset [get_filesets sources_1] $files
 
 set_property verilog_define [list FPGA Differential_clock_capable_pin] [get_filesets sources_1] 
@@ -135,9 +124,6 @@ set_property -dict [list \
 	[get_ips axi_uartlite_0]
 generate_target {instantiation_template} [get_files $proj_dir/$project_name.srcs/sources_1/ip/axi_uartlite_0/axi_uartlite_0.xci]
 
-
-
-
 # Create 'constrs_1' fileset (if not found)
 if {[string equal [get_filesets -quiet constrs_1] ""]} {
   create_fileset -constrset constrs_1
@@ -147,7 +133,7 @@ if {[string equal [get_filesets -quiet constrs_1] ""]} {
 set obj [get_filesets constrs_1]
 
 # Add/Import constrs file and set constrs file properties
-set file "[file normalize "$bsp_dir/board/$target/constraint/$target.xdc"]"
+set file "[file normalize "$bsp_dir/board/$board/constraint/ioportmap.xdc"]"
 set file_added [add_files -norecurse -fileset $obj $file]
 
 # generate all IP source code
