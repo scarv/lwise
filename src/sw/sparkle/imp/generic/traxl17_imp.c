09f43c477ee0a8f982567b6319f8ccbb427067ee
Dan Page <dan@phoo.org>
2021-09-27 19:25:43 +0100

    readd missing files

---
+++ src/sw/sparkle/imp/generic/traxl17_imp.c
@@ -0,0 +1,8 @@
+/* Copyright (C) 2021 SCARV project <info@scarv.org>
+ *
+ * Use of this source code is restricted per the MIT license, a copy of which 
+ * can be found at https://opensource.org/licenses/MIT (or should be included 
+ * as LICENSE.txt within the associated archive or repository).
+ */
+
+#include "traxl17_imp.h"
