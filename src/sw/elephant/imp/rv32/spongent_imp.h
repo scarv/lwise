c38f4eb9548be7f5ef98f398eed521858f80010d
Hao Cheng <50016026+ulhaocheng@users.noreply.github.com>
2022-03-02 15:53:58 +0100

    typo

--- src/sw/elephant/imp/rv32/spongent_imp.h
+++ src/sw/elephant/imp/rv32/spongent_imp.h
@@ -7,8 +7,9 @@ extern void Spongent160_5x32b(void *state);
 #define Spongent_160 Spongent160_5x32b
 
 // Discussions 
-// 4x40b asm: AddRC(table) + pLayer(swapmove) + sBoxLayer(bitsliced) -> doesn't need ZBKX
-// 5x32b asm: AddRC(table) + sBoxLayer(table) + pLayer(swapmove)     ->         need ZBKX
+// 4x40b asm: AddRC(table) + pLayer(swapmove) + sBoxLayer(bitsliced) -> doesn't need  ZBKX -> also useful for cortex-M 
+// 5x32b asm: AddRC(table) + sBoxLayer(table) + pLayer(swapmove)     ->         needs ZBKX
+
 
 // Cycles: 
 // 4x40b asm: 143257
