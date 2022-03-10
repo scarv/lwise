8b833b5b5fb410633857bcfc9bc3da284ec6da49
Hao Cheng <50016026+ulhaocheng@users.noreply.github.com>
2022-03-10 12:55:49 +0100

    Update Elephant

--- src/sw/elephant/imp/rv32/spongent_imp.h
+++ src/sw/elephant/imp/rv32/spongent_imp.h
@@ -7,7 +7,7 @@ extern void Spongent160_5x32b(void *state);
 #if (ELEPHANT_STATE_5X32B)
 #define Spongent_160 Spongent160_5x32b
 #else
-// use 4x40b imp by default
+// Use 4x40b state representation by default
 #define Spongent_160 Spongent160_4x40b
 #endif 
 
