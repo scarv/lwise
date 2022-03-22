77782b5a3e81eaca9b11f62d0b54de987d0206bb
phthinh <hung3@e.ntu.edu.sg>
2022-03-22 14:35:14 +0000

    update hw makefiles

--- bin/size.py
+++ bin/size.py
@@ -8,20 +8,20 @@ func_name = sys.argv[2:]
 
 asmfp = open(asmfile, 'r')
 
-print (func_name)
+#print (func_name)
 line = asmfp.readline()
 while line != "":
     p =  scanf("%s <%s>:", line)
     if p != None:
         if str(p[1]) in func_name:
-            print(str(line))
+            print(str(line[0:-1]))
             start = int(p[0],16)
             line = asmfp.readline()
             while line != "":
                 p =  scanf("%s <%s>:", line)
                 if p != None:
                     print("size:" + str(int(p[0],16)-start) + "bytes")
-                    print("next function -> " + str(line))
+                    #print("next function -> " + str(line))
                     break
                 line = asmfp.readline()
             continue
