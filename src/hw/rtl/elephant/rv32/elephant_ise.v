944837af5c982af637aee0a9c31e2439c48e402b
phthinh <hung3@e.ntu.edu.sg>
2022-04-07 09:41:22 +0100

    update elephant ise rtl

--- src/hw/rtl/elephant/rv32/elephant_ise.v
+++ src/hw/rtl/elephant/rv32/elephant_ise.v
@@ -9,43 +9,20 @@ input  wire         op_pstep2_y,
 
 output wire [31:0]  rd
 );
+ 
+`define swapmv(x, y, msk, N, x_y, t)   (x_y) ? (x ^ { t[31-N:0], {N{1'b0}} }) : (y ^ t); \
+        assign                           t   = (y ^ { {N{1'b0}},  x[31:N]  }) & msk    
 
-`define swapmv(x, xshf, y, msk, t, tshf, x_y)   (x_y) ? (x ^ tshf): (y ^ t); \
-        assign  t     = (y ^ xshf) & msk        
+wire [31:0] t0, t1, t2, t3, t4, t5, t6;
 
-wire [31:0] x_rsh08 = { 8'd0,rs1[31: 8]};
-wire [31:0] x_rsh16 = {16'd0,rs1[31:16]};
-wire [31:0] x_rsh24 = {24'd0,rs1[31:24]};
+wire [31:0] pstep2_imm0 = `swapmv(rs1, rs2, 32'h000000FF,  8, op_pstep2_x, t0);
+wire [31:0] pstep2_imm1 = `swapmv(rs1, rs2, 32'h000000FF, 16, op_pstep2_x, t1);
+wire [31:0] pstep2_imm2 = `swapmv(rs1, rs2, 32'h000000FF, 24, op_pstep2_x, t2);
+wire [31:0] pstep2_imm3 = `swapmv(rs1, rs2, 32'h0000FF00,  8, op_pstep2_x, t3);
 
-wire [31:0] t_imm0;
-wire [31:0] t0_lsh = {t_imm0[23:0],  8'd0};
-
-wire [31:0] t_imm1;
-wire [31:0] t1_lsh = {t_imm1[15:0], 16'd0};
-
-wire [31:0] t_imm2;
-wire [31:0] t2_lsh = {t_imm2[ 7:0], 24'd0};
-
-wire [31:0] t_imm3;
-wire [31:0] t3_lsh = {t_imm3[23:0],  8'd0};
-
-wire [31:0] t_imm4;
-wire [31:0] t4_lsh = {t_imm4[ 7:0], 24'd0};
-
-wire [31:0] t_imm5;
-wire [31:0] t5_lsh = {t_imm5[15:0], 16'd0};
-
-wire [31:0] t_imm6;
-wire [31:0] t6_lsh = {t_imm6[23:0],  8'd0};
-
-wire [31:0] pstep2_imm0 = `swapmv(rs1, x_rsh08, rs2, 32'h000000FF, t_imm0, t0_lsh, op_pstep2_x);
-wire [31:0] pstep2_imm1 = `swapmv(rs1, x_rsh16, rs2, 32'h000000FF, t_imm1, t1_lsh, op_pstep2_x);
-wire [31:0] pstep2_imm2 = `swapmv(rs1, x_rsh24, rs2, 32'h000000FF, t_imm2, t2_lsh, op_pstep2_x);
-wire [31:0] pstep2_imm3 = `swapmv(rs1, x_rsh08, rs2, 32'h0000FF00, t_imm3, t3_lsh, op_pstep2_x);
-
-wire [31:0] pstep2_4    = `swapmv(rs1, x_rsh24, rs2, 32'h000000FF, t_imm4, t4_lsh, op_pstep2_x);
-wire [31:0] pstep2_5    = `swapmv(rs1, x_rsh16, rs2, 32'h0000FF00, t_imm5, t5_lsh, op_pstep2_x);
-wire [31:0] pstep2_6    = `swapmv(rs1, x_rsh08, rs2, 32'h00FF0000, t_imm6, t6_lsh, op_pstep2_x);
+wire [31:0] pstep2_4    = `swapmv(rs1, rs2, 32'h000000FF, 24, op_pstep2_x, t4);
+wire [31:0] pstep2_5    = `swapmv(rs1, rs2, 32'h0000FF00, 16, op_pstep2_x, t5);
+wire [31:0] pstep2_6    = `swapmv(rs1, rs2, 32'h00FF0000,  8, op_pstep2_x, t6);
 
 wire [31:0] pstep2_imm4 = (op_pstep2_y)? pstep2_4: {pstep2_4[23:0], pstep2_4[31:24]};
 wire [31:0] pstep2_imm5 = (op_pstep2_y)? pstep2_5: {pstep2_5[15:0], pstep2_5[31:16]};
