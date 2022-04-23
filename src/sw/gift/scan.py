fda2b0315666629bc61642582404daea04149437
Hao Cheng <50016026+ulhaocheng@users.noreply.github.com>
2022-04-23 18:27:35 +0200

    GIFT: rename BS TYPE2 -> TYPE3; update TYPE3

--- src/sw/gift/scan.py
+++ src/sw/gift/scan.py
@@ -15,8 +15,14 @@ def rv32( args ) :
     driver.run( args, 'gift', CONF, 'rv32', 'nist', NIST_IMP = 'ref' )
 
   for TYPE in [ 'GIFT_RV32_TYPE1', 'GIFT_RV32_TYPE2' ] :
-    for METHOD in [ 'GIFT_BITSLICING', 'GIFT_FIXSLICING'] :
-      CONF = [ TYPE, METHOD ]
+      CONF  = [ TYPE, 'GIFT_FIXSLICING' ]
+      # there is only unrolled implementation for GIFT-COFB on rv32
+      CONF += ['GIFT_RV32_UNROLL']
+
+      driver.run( args, 'gift', CONF, 'rv32', 'rv32' )
+
+  for TYPE in [ 'GIFT_RV32_TYPE1', 'GIFT_RV32_TYPE3' ] :
+      CONF  = [ TYPE, 'GIFT_BITSLICING' ]
       # there is only unrolled implementation for GIFT-COFB on rv32
       CONF += ['GIFT_RV32_UNROLL']
 
